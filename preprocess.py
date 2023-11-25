1f8b83d0c86d692089183497e85f964fac4f7011
Simon Zhang <zhangshi0512@gmail.com>
2023-11-24 22:48:01 -0800

    Preprocess Test

--- preprocess.py
+++ preprocess.py
@@ -3,18 +3,37 @@
 # This file resize the image for uniformity
 # and apply filters
 
+import os
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-def load_and_preprocess_image(path):
-    image = cv2.imread(path)
-    image = cv2.resize(image, (800, 600))  # Resize for uniformity
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+def preprocess_image(image):
+    resized_image = cv2.resize(image, (800, 600))  # Resize for uniformity
+    gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
     return gray_image
 
-# Example usage
-image = load_and_preprocess_image('path_to_image.jpg')
-plt.imshow(image, cmap='gray')
-plt.show()
+def process_images_in_folder(folder_path, output_folder=None):
+    print(f"Processing images in folder: {folder_path}")
+    for filename in os.listdir(folder_path):
+        if filename.lower().endswith(('.jpg', '.jpeg')):
+            file_path = os.path.join(folder_path, filename)
+            print(f"Processing {file_path}...")
+            image = cv2.imread(file_path)
+            processed_image = preprocess_image(image)
+
+            if output_folder:
+                os.makedirs(output_folder, exist_ok=True)
+                output_path = os.path.join(output_folder, filename)
+                cv2.imwrite(output_path, processed_image)
+                print(f"Saved processed image to {output_path}")
+
+            # Uncomment below lines to view the images
+            cv2.imshow("Processed Image", processed_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    process_images_in_folder('images/blaine_house/', 'preprocessed_images/blain_house/')
+
+
 
